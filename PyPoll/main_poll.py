1f736efd5f6c7cd250685c3061693d9ca9b9bef0
Casey Juergens <79878212+caseyjuergens@users.noreply.github.com>
2021-04-07 23:01:41 -0700

    trying out a conditional

--- PyPoll/main_poll.py
+++ PyPoll/main_poll.py
@@ -8,6 +8,9 @@ file_output = os.path.join("Analysis", "pypoll_analysis.txt")
 
 #global variables
 total_votes = 0
+# empty lists for candidates and their vote counts
+individual_candidates = []
+votes_for_candidates =[]
 
 #open and read budget data
 with open(pypoll_csv) as csvfile:
@@ -21,17 +24,32 @@ with open(pypoll_csv) as csvfile:
         #find total number of votes cast
         total_votes +=1
 
+        # complete list of candidates
+        candidates = row[2] 
+
+        # if we come across a vote for a candidate who is not already in our individual list
+        if candidates not in individual_candidates: 
+
+            # use append to add them to indiviual_candidates 
+            individual_candidates.append(candidates)
+
+            # start counting votes for that individual candidate
+            votes_for_candidates[individual_candidates] = 0
+
+            # add 1 to votes_for_candidates
+            
+       
+
+        # percentage of votes each candidate won
 
 
-
-
-
+        # find the winner
 
 
 output= (
     f'Election Results\n'
     f'----------------------------\n'
-    f'Total Votes: \n'
+    f'Total Votes: {total_votes} \n'
     f'----------------------------\n'
     f'Khan: \n'
     f'Correy: \n'
