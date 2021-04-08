62e88554fd885f77e23cdaf29b03aea6b3dfd0d1
Casey Juergens <caseyjuergens@gmail.com>
2021-04-07 20:48:04 -0700

    finished increase and decrease func

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -10,6 +10,7 @@ file_output = os.path.join("Analysis", "pybank_analysis.txt")
 total_months = 0
 total_net = 0
 
+
 #open and read budget data
 with open(Pybank_csv) as csvfile:
     csvreader = csv.reader(csvfile)
@@ -19,19 +20,32 @@ with open(Pybank_csv) as csvfile:
     
     #loop thru rows in budgetdata
     for row in csvreader:
-                                                                                      
+        #find number of total months                                                                              
         total_months +=1
-        total_net= total_net + int(row[1])
 
+        #find  net total of profit/losses of entire period
+        total_net = total_net + int(row[1])
+
+        #calculate the changes in prof/loss period, then find average
+        average_change = total_net / total_months
+
+        #find greatest increase in profits (dates and amounts)
+        rows= list(csvreader)
+        greatest_increase = max(rows, key=lambda row: int(row[1]))
+        greatest_decrease = min(rows, key=lambda row: int(row[1]))
+        
+        #find greatest decrease in losses (dates and amounts)
+        
+       
 
 output= (
     f'Financial Analysis\n'
     f'----------------------------\n'
     f'Total months: {total_months}\n'
-    f'Net total: {total_net}\n'
-    f'Average change: \n'
-    f'Greatest Increase in Profit: \n'
-    f'Greatest Decrease in Profit: \n'
+    f'Net total: ${total_net}\n'
+    f'Average change: ${round(average_change)}\n'
+    f'Greatest Increase in Profit: ${greatest_increase}\n'
+    f'Greatest Decrease in Profit: {greatest_decrease}\n'
 )
 
     #outside loop
