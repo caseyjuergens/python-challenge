ef1ee2ff0350f4164a8987416f4a79b5d216bd51
Casey Juergens <caseyjuergens@gmail.com>
2021-04-08 22:59:32 -0700

    cleaned up file

--- PyBank/main_bank.py
+++ PyBank/main_bank.py
@@ -10,7 +10,6 @@ file_output = os.path.join("Analysis", "pybank_analysis.txt")
 total_months = 0
 total_net = 0
 
-
 #open and read budget data
 with open(Pybank_csv) as csvfile:
     csvreader = csv.reader(csvfile)
@@ -29,26 +28,22 @@ with open(Pybank_csv) as csvfile:
         #calculate the changes in prof/loss period, then find average
         average_change = total_net / total_months
 
-        #find greatest increase in profits (dates and amounts)
+        #find greatest increase/decrease in profits (dates and amounts)
         rows= list(csvreader)
         greatest_increase = max(rows, key=lambda row: int(row[1]))
         greatest_decrease = min(rows, key=lambda row: int(row[1]))
-        
-        #find greatest decrease in losses (dates and amounts)
-        
        
-
-output= (
+output=(
     f'Financial Analysis\n'
     f'----------------------------\n'
     f'Total months: {total_months}\n'
     f'Net total: ${total_net}\n'
     f'Average change: ${round(average_change)}\n'
-    f'Greatest Increase in Profit: ${greatest_increase}\n'
+    f'Greatest Increase in Profit: {greatest_increase}\n'
     f'Greatest Decrease in Profit: {greatest_decrease}\n'
 )
 
-    #outside loop
+#outside loop
 print(output)
 with open(file_output, "w") as txt_file:
     txt_file.write(output)
